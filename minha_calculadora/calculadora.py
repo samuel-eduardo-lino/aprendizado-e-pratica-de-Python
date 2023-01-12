d72b8992d2b9c88374aaff29ffcb864eddd5ec44
samuel-eduardo <samuellino2006@gmail.com>
2023-01-12 19:41:09 -0300

    atualização

--- minha_calculadora/calculadora.py
+++ minha_calculadora/calculadora.py
@@ -14,11 +14,13 @@ def digitar(num:str):
     global number
     if number != '':
         number = number + num
-        operacao.config(text= f'{number}')
+        operacao.delete(0, END)
+        operacao.insert(0, number)
         
     else:
         number= num
-        operacao.config(text=f'{number}')
+        operacao.delete(0, END)
+        operacao.insert(0, number)
     
     
     
@@ -150,21 +152,24 @@ def calcular(texto:str):
         prime = float(separado[0])
         secun = float(separado[1])
         resultado = prime + secun
-        operacao.config(text=f'{number} = {resultado}')
+        operacao.delete(0, END)
+        operacao.insert(0, f'{number} = {resultado}')
         calculou = True
     elif texto.find('/') != -1:
         separado = texto.split('/')
         prime = float(separado[0])
         secun = float(separado[1])
         resultado = prime / secun
-        operacao.config(text=f'{number} = {resultado}')
+        operacao.delete(0, END)
+        operacao.insert(0, f'{number} = {resultado}')
         calculou = True
     elif texto.find('*') != -1:
         separado = texto.split('*')
         prime = float(separado[0])
         secun = float(separado[1])
         resultado = prime * secun
-        operacao.config(text=f'{number} = {resultado}')
+        operacao.delete(0, END)
+        operacao.insert(0, f'{number} = {resultado}')
         calculou = True
     elif texto.find('-') != -1:
         separado = texto.split('-')
@@ -176,7 +181,8 @@ def calcular(texto:str):
             prime = float(separado[0])
             secun = float(separado[1])
             resultado = prime - secun
-        operacao.config(text=f'{number} = {resultado}')
+        operacao.delete(0, END)
+        operacao.insert(0, f'{number} = {resultado}')
         calculou = True
     else:
         return
@@ -202,14 +208,29 @@ def apagar_anterior():
     posi = tamanho - 1
     novo_number = number[0:posi]
     number = novo_number
-    operacao.config(text=number)
+    operacao.delete(0, END)
+    operacao.insert(0, number)
 
 
 def raiz():
     global number
     if number.isnumeric():
         resultado = sqrt(int(number))
-        operacao.config(text=f'\u221A{number} = {resultado}')
+        operacao.delete(0, END)
+        operacao.insert(0, f'\u221A{number} = {resultado}')
+        number = ''
+
+
+def fatorial():
+    global number
+    if number.isnumeric():
+        valor = int(number)
+        resultado = int(number)
+        for n in range(1, valor):
+            resultado = resultado * (valor - 1)
+            valor -= 1
+        operacao.delete(0, END)
+        operacao.insert(0, f'{number}! = {resultado}')
         number = ''
 
 
@@ -217,44 +238,52 @@ janela = Tk()
 janela.title('Calculadora')
 
 
-operacao = Label(janela, width=30, height=2)
+
+operacao = Entry(janela, text='', width=40)
 operacao.grid(row=0, column=0, columnspan=5)
+scroll = Scrollbar(janela, orient='horizontal', width=10)
+scroll.grid(row=1, column=0)
+operacao.config(xscrollcommand=scroll.set)
+scroll.config(command=operacao.xview)
 num1 = Button(janela, text='1', height=2, width=5, command=numero1)
-num1.grid(row=1, column=0)
+num1.grid(row=2, column=0)
 num2 = Button(janela, text='2', height=2, width=5, command=numero2)
-num2.grid(row=1, column=1)
+num2.grid(row=2, column=1)
 num3 = Button(janela, text='3', height=2, width=5, command=numero3)
-num3.grid(row=1, column=2)
+num3.grid(row=2, column=2)
 num4 = Button(janela, text='4', height=2, width=5, command=numero4)
-num4.grid(row=2, column=0)
+num4.grid(row=3, column=0)
 num5 = Button(janela, text='5', height=2, width=5, command=numero5)
-num5.grid(row=2, column=1)
+num5.grid(row=3, column=1)
 num6 = Button(janela, text='6', height=2, width=5, command=numero6)
-num6.grid(row=2, column=2)
+num6.grid(row=3, column=2)
 num7 = Button(janela, text='7', height=2, width=5, command=numero7)
-num7.grid(row=3, column=0)
+num7.grid(row=4, column=0)
 num8 = Button(janela, text='8', height=2, width=5, command=numero8)
-num8.grid(row=3, column=1)
+num8.grid(row=4, column=1)
 num9 = Button(janela, text='9', height=2, width=5, command=numero9)
-num9.grid(row=3, column=2)
+num9.grid(row=4, column=2)
 num0 = Button(janela, text='0', height=2, width=5, command=numero0)
-num0.grid(row=4, column=1)
+num0.grid(row=5, column=1)
 sinmais = Button(janela, text='+', height=2, width=5, command=mais)
-sinmais.grid(row=4, column=3)
+sinmais.grid(row=5, column=3)
 sinmenos = Button(janela, text='-', height=2, width=5, command=menos)
-sinmenos.grid(row=3, column=3)
+sinmenos.grid(row=4, column=3)
 sindivi = Button(janela, text='/', height=2, width=5, command=divi)
-sindivi.grid(row=2, column=3)
+sindivi.grid(row=3, column=3)
 sinmulti = Button(janela, text='*', height=2, width=5, command=multi)
-sinmulti.grid(row=1, column=3)
+sinmulti.grid(row=2, column=3)
 pont = Button(janela, text='.', height=2, width=5, command=ponto)
-pont.grid(row=4, column=0)
+pont.grid(row=5, column=0)
 igual = Button(janela, text='=', height=2, width=5, command=igualar)
-igual.grid(row=4, column=2)
+igual.grid(row=5, column=2)
 apagar = Button(janela, text='<-', height=2, width=5, command=apagar_anterior)
-apagar.grid(row=1, column=4)
+apagar.grid(row=2, column=4)
 raizq = Button(janela, text='\u221A', height=2, width=5, command=raiz)
-raizq.grid(row=2, column=4)
+raizq.grid(row=3, column=4)
+fatori = Button(janela, text='!', height=2, width=5, command=fatorial)
+fatori.grid(row=4, column=4)
+
 
 
 
